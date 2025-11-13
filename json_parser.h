87e89e3449cf5efb1e9794d77b5579343c4f56c2
AbHXn <asofficial47@gmail.com>
2025-11-13 14:38:09 +0530

    Update json_parser.h

--- json_parser.h
+++ json_parser.h
@@ -7,12 +7,19 @@
 #include 	<stdio.h>
 #include 	<stdlib.h>
 
-
 #define 	MAX_KEY_SIZE 	250
 #define 	MAX_VALUE_SIZE	1024
 #define 	INDEX_KEY_LEN 	10
 #define 	BUFFER_SIZE 	250
 #define 	MAX_SPACE 		100
+
+typedef enum status_flags INPUT_STATUS;
+
+#define is_key_filled(flag) 	  ((flag) & KEY_ENTERED)
+#define is_value_filled(flag) 	  ((flag) & VALUE_ENTERED)
+#define is_in_filling_mode(flag)  ((flag) & SOMETHING_INPUTING)
+#define is_list_filling(flag) 	  ((flag) & LIST_INPUTING)
+#define turn_on_off(flag1, flag2) ((flag1) ^ (flag2))
 
 // buffer is int to track of EOF
 extern int 		BUFFER[BUFFER_SIZE];
@@ -29,11 +36,11 @@ typedef enum {
 	COMMA 		= ','
 }J_SYNTAX;
 
-typedef enum {
+typedef enum status_flags{
 	SOMETHING_INPUTING		= 1 << 0,
 	VALUE_ENTERED			= 1 << 1,
-	KEY_IS_ENTERED			= 1 << 2,
-	INPUTING_LIST			= 1 << 3
+	KEY_ENTERED 			= 1 << 2,
+	LIST_INPUTING			= 1 << 3
 }INPUT_STATUS;
 
 typedef struct next_children{
@@ -57,13 +64,8 @@ bool push_char_buffer	( const int c );
 bool push_buffer 		( const char* buf );
 char _fgetc 			( void );
 
-bool is_key_filled		( int _flag );
-bool is_value_filled	( int _flag );
-bool is_in_filling_mode	( int _flag );
 bool is_value_filling	( int _flag );
 bool is_value_pair		( int _flag );
-bool is_list_inputing	( int _flag );
-int  turn_on_off		( int _flag, int _t_flag );
 
 bool is_json_syntax			( char c );
 void add_child_to_parent	( Node* parent, Node *child );
