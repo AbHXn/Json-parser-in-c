3d51a8e1f3a3ddfd0bf2da2caba3b66b67387e79
AbHXn <asofficial47@gmail.com>
2025-11-13 12:53:34 +0530

    Add json_parser.h header file for JSON parsing

---
+++ json_parser.h
@@ -0,0 +1,76 @@
+#ifndef JSON_PARSER_H
+#define JSON_PARSER_H
+
+#include 	<string.h>
+#include 	<ctype.h>
+#include 	<stdbool.h>
+#include 	<stdio.h>
+#include 	<stdlib.h>
+
+
+#define 	MAX_KEY_SIZE 	250
+#define 	MAX_VALUE_SIZE	1024
+#define 	INDEX_KEY_LEN 	10
+#define 	BUFFER_SIZE 	250
+#define 	MAX_SPACE 		100
+
+// buffer is int to track of EOF
+extern int 		BUFFER[BUFFER_SIZE];
+extern int 		buffer_ptr;
+extern FILE* 	JSON_FILE;
+
+typedef enum {
+	OPEN_C 		= '{',
+	CLOSE_C		= '}',
+	OPEN_S 		= '[',
+	CLOSE_S 	= ']',
+	APPO 		= '"',
+	SEMI	 	= ':',
+	COMMA 		= ','
+}J_SYNTAX;
+
+typedef enum {
+	SOMETHING_INPUTING		= 1 << 0,
+	VALUE_ENTERED			= 1 << 1,
+	KEY_IS_ENTERED			= 1 << 2,
+	INPUTING_LIST			= 1 << 3
+}INPUT_STATUS;
+
+typedef struct next_children{
+	struct _node*		  child;
+	struct next_children* next;
+}ChD;
+
+typedef struct _node{
+	char*	key;
+   	char*	value;
+	ChD*	childrens;
+}Node;
+
+static const char* FILE_NOT_PROVIDED	 = "Provide Json File\n";
+static const char* TREE_ALLOC_ERROR 	 = "Failed in tree creation\n";
+static const char* BUFFER_LIMITED_ERROR  = "Buffer is full\n";
+static const char* KEY_VALUE_ALLOC_ERROR = "Failed to create key/value\n";
+
+
+bool push_char_buffer	( const int c );
+bool push_buffer 		( const char* buf );
+char _fgetc 			( void );
+
+bool is_key_filled		( int _flag );
+bool is_value_filled	( int _flag );
+bool is_in_filling_mode	( int _flag );
+bool is_value_filling	( int _flag );
+bool is_value_pair		( int _flag );
+bool is_list_inputing	( int _flag );
+int  turn_on_off		( int _flag, int _t_flag );
+
+bool is_json_syntax			( char c );
+void add_child_to_parent	( Node* parent, Node *child );
+void traverse_tree			( Node *root, char *prefix, int is_last );
+void recursivily_build		( Node *parent, int _flag );
+void free_entire_tree		( Node *root );
+Node *get_new_node			( const char *key, const char *value );
+
+
+#endif // JSON_PARSER_H
